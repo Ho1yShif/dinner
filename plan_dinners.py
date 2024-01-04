b066fa11884042ff411d2d76dac2381e42899f4f
Shifra Isaacs <shifraisaacs@gmail.com>
2024-01-03 22:20:24 -0500

    Add veg lists

--- plan_dinners.py
+++ plan_dinners.py
@@ -1,4 +1,11 @@
 staples = ["garlic", "olive oil", "neutral oil",
 		   "salt", "pepper", "vegan butter",
 		   "eggs", "mayonnaise", "vegetable broth",
-		   "vegan parmesan"]
+		   "vegan parmesan", "vegan cheese"]
+
+fresh_veg = ["spinach", "carrots", "asparagus",
+			  "orange peppers", "yellow peppers", "cabbage",
+			  "zucchini", "squash", "cucumber",
+			  "snap peas", "brussels sprouts"]
+
+frozen_veg = ["broccoli", "cauliflower", "peas", "green beans"]

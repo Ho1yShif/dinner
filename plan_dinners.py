4d93df019c0380ca96392a815dc4b0714b19367b
Shifra Isaacs <shifraisaacs@gmail.com>
2024-01-04 00:13:39 -0500

    Add chia

--- plan_dinners.py
+++ plan_dinners.py
@@ -27,7 +27,7 @@ class PlanDinners:
 		self.staples = ["garlic", "olive oil", "neutral oil",
 				"salt", "pepper", "vegan butter",
 				"eggs", "mayonnaise", "vegetable broth",
-				"vegan parmesan", "vegan cheese"]
+				"vegan parmesan", "vegan cheese", "chia seeds"]
 		self.staples.sort()
 
 		self.fresh_veg = ["spinach", "carrots", "asparagus",

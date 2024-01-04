6f2a0e8c2a82b25b3f3d2c02e1884f17954a61cc
Shifra Isaacs <shifraisaacs@gmail.com>
2024-01-03 22:43:50 -0500

    Meal plan functionality

--- plan_dinners.py
+++ plan_dinners.py
@@ -1,7 +1,8 @@
 import json
+import random
 from typing import List
 from datetime import date
-
+import pandas as pd
 
 class plan_dinners:
 	"""
@@ -9,9 +10,13 @@ class plan_dinners:
 	Display plan and shopping list in a user-friendly format using prints
 	Then build a UI for the menu planning and shopping list
 	"""
-	def __init__(self, meals:List[str]):
-		self.meals = meals
+
+	def __init__(self):
 		self.today = str(date.today()).replace("-", "_")
+
+		"""Read dinners from JSON"""
+		self.dinners = json.load(open("dinners.json", "r"))
+		self.dinners = dict(self.dinners)
 
 		"""Set staple lists"""
 		self.staples = ["garlic", "olive oil", "neutral oil",
@@ -27,4 +32,33 @@ class plan_dinners:
 		self.frozen_veg = ["broccoli", "cauliflower", "peas", "green beans"]
 
 		self.toppings = ["hemp seeds", "pumpkin seeds", "avocado", "cottage cheese"]
+
+
+	def menu(self):
+		meal_options = list(self.dinners.keys())
+		remaining_meals = 3
+		chosen_meals = []
+
+		"""Randomly select 3 meals and display menu"""
+		random_meals = random.choices(sequence=meal_options, k=remaining_meals)
+		chosen_meals.extend(random_meals)
+		random.shuffle(chosen_meals)
+
+		print(f"Length of chosen meals: {len(chosen_meals)}")
+
+		meal_schedule = {
+			"Monday": chosen_meals[0],
+			"Tuesday": chosen_meals[1],
+			"Wednesday": chosen_meals[2]
+		}
+
+		print(f"Menu for the week of {self.today}:")
+		df = pd.DataFrame(columns=["Day", "Meal", "Ingredients", "Prep"])
+		for day, meal in meal_schedule.items():
+			ingredients = self.dinners[meal]["ingredients"]
+			prep = self.dinners[meal]["prep"]
+			df = df.append({"Day": day, "Meal": meal, "Ingredients": ingredients, "Prep": prep}, ignore_index=True)
+
+		print(df)
+
 

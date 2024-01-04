eb861f532b39e9dfcc6dbea6a676c57a62a0e584
Shifra Isaacs <shifraisaacs@gmail.com>
2024-01-03 22:27:19 -0500

    Build plan_dinners class

--- plan_dinners.py
+++ plan_dinners.py
@@ -1,11 +1,30 @@
-staples = ["garlic", "olive oil", "neutral oil",
-		   "salt", "pepper", "vegan butter",
-		   "eggs", "mayonnaise", "vegetable broth",
-		   "vegan parmesan", "vegan cheese"]
+import json
+from typing import List
+from datetime import date
 
-fresh_veg = ["spinach", "carrots", "asparagus",
-			  "orange peppers", "yellow peppers", "cabbage",
-			  "zucchini", "squash", "cucumber",
-			  "snap peas", "brussels sprouts"]
 
-frozen_veg = ["broccoli", "cauliflower", "peas", "green beans"]
+class plan_dinners:
+	"""
+	Plan 3 weekly dinners
+	Display plan and shopping list in a user-friendly format using prints
+	Then build a UI for the menu planning and shopping list
+	"""
+	def __init__(self, meals:List[str]):
+		self.meals = meals
+		self.today = str(date.today()).replace("-", "_")
+
+		"""Set staple lists"""
+		self.staples = ["garlic", "olive oil", "neutral oil",
+				"salt", "pepper", "vegan butter",
+				"eggs", "mayonnaise", "vegetable broth",
+				"vegan parmesan", "vegan cheese"]
+
+		self.fresh_veg = ["spinach", "carrots", "asparagus",
+					"orange peppers", "yellow peppers", "cabbage",
+					"zucchini", "squash", "cucumber",
+					"snap peas", "brussels sprouts"]
+
+		self.frozen_veg = ["broccoli", "cauliflower", "peas", "green beans"]
+
+		self.toppings = ["hemp seeds", "pumpkin seeds", "avocado", "cottage cheese"]
+
